70661b9258459823c2b5f31a47ab7a4227c0b0ed
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-09-17 10:19:25 -0600

    'Commit'

--- styles.js
+++ styles.js
@@ -15,3 +15,30 @@ function goodbye(){
 repeat(hello, 5);
 repeat(goodbye, 5);
 
+function filter(arr, fn) {
+    let newArray = [];
+    for(let item of arr) {
+        if(fn(item)){
+            newArray.push(item);
+        }
+    }
+    return newArray;
+}
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+const filteredNames = filter(myNames, (name) => name[0] === 'R');
+
+console.log(filteredNames);
+
+function hazardWarningCreator(typeOfWarning) {
+    let warningCounter = 0;
+    return (location) => {
+        warningCounter++;
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time${(warningCounter === 1) ? '' : 's'} today!`);
+    };
+}
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+
+let turtleMomvent = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
