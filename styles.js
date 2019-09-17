68d132b2d51330429371efc2c395ea85f4248986
Nathan Leung <nath.leung@gmail.com>
2019-09-17 09:10:27 -0700

    Fucntions as arguments (1)

--- styles.js
+++ styles.js
@@ -1,3 +1,17 @@
 function repeat(fn,n){ 
-    for (let x = 0; x < n)
+    for (let x = 0; x < n; x++){
+        fn();
+    }
 }
+
+function hello(){
+    console.log('Hello World');
+}
+
+function goodbye(){
+    console.log('Goodbye world');
+}
+
+repeat(hello, 5);
+repeat(goodbye, 5);
+
